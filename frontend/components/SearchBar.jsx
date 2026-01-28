521a601a60ef599e9cdaab4e30e63e190ff0ef2a
Nitin Jangid <nitinjangidj92@gmail.com>
2026-01-28 16:18:32 +0530

    Update SearchBar.jsx

--- frontend/components/SearchBar.jsx
+++ frontend/components/SearchBar.jsx
@@ -4,7 +4,7 @@ export default function SearchBar({ onSearch }) {
   return (
     <div className="mb-4">
     <input
-      className="w-[350] p-3 bg-zinc-900 border border-zinc-700 rounded-3xl"
+      className="w-[320] p-3 bg-zinc-900 border border-zinc-700 rounded-3xl"
       placeholder="Search YouTube..."
       onKeyDown={(e) => {
         if (e.key === "Enter") onSearch(e.target.value);
