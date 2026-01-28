aaba2fddb11672eba4474200442805936037e7d6
Nitin Jangid <nitinjangidj92@gmail.com>
2026-01-28 16:12:48 +0530

    Update axiosInstance.js

--- frontend/data/axiosInstance.js
+++ frontend/data/axiosInstance.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL:
+    process.env.NEXT_PUBLIC_BASE_URL ||
+    "https://youtube-clone-eosin-five.vercel.app",
 });
 
 export default axiosInstance;
