32d136fec7028c1b847aaff92b35905bafff88a5
Nitin Jangid <nitinjangidj92@gmail.com>
2026-01-28 22:28:46 +0530

    Update savedVideoController.js

--- backend/controllers/savedVideoController.js
+++ backend/controllers/savedVideoController.js
@@ -5,22 +5,18 @@ import AppError from "../utils/AppError.js";
 export const getSavedVideos = catchAsync(async (req, res) => {
   res.json(await SavedVideo.find());
 });
-
 export const saveVideo = catchAsync(async (req, res) => {
   const video = await SavedVideo.findOneAndUpdate(
     { videoId: req.body.videoId },
     req.body,
     { upsert: true, new: true }
   );
-
   res.json(video);
 });
-
 export const deleteSavedVideo = catchAsync(async (req, res) => {
   const deleted = await SavedVideo.findOneAndDelete({
     videoId: req.params.videoId,
   });
-
   if (!deleted)
     throw new AppError("Video not found", 404);
 
